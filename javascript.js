f80ac650b8ac321773df97dda40cfd38cd9c06c9
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-07-01 01:58:16 -0300

    teste 2

--- javascript.js
+++ javascript.js
@@ -1,13 +1,25 @@
+//  Importando o módulo fs, utilizado para leitura e escrita de arquivos
 const fs = require('fs');
 
-const filename = 'biblioteca.txt';
-let biblioteca = [];
+let biblioteca = []; // Armazenar os livros cadastrados.
 
-function cadastrarLivro() {
-  console.log('======== Cadastro de Livro =======');
-  const titulo = prompt('Digite o título do livro:');
-  const autor = prompt('Digite o autor do livro:');
-  const data = prompt('Digite a data de publicação do livro (formato: DD/MM/AAAA):');
+// Função para exibir o menu de opções do Gerenciador de livros
+function menu() {
+  console.log('//----- Gerenciador de livros -----//');
+  console.log('1 -> Cadastrar um livro');
+  console.log('2 -> Listar os livros cadastrados');
+  console.log('3 -> Alterar detalhes de um livro');
+  console.log('4 -> Remover um livro da Biblioteca');
+  console.log('5 -> Sair do programa');
+  console.log('-----------------------------------\n');
+}
+
+// Função para cadastrar um livro 
+function cadastro() {
+  console.log('//----- Cadastro de Livro -----//');
+  const titulo = prompt('Insira o título do livro:');
+  const autor = prompt('Insira o autor do livro:');
+  const data = prompt('Insira a data de publicação do livro:');
 
   const livro = { titulo, autor, data };
   biblioteca.push(livro);
@@ -15,29 +27,36 @@ function cadastrarLivro() {
   salvarBiblioteca();
 
   console.log('Livro cadastrado com sucesso!');
-  console.log('================================\n');
+  console.log('------------------------------\n');
 }
 
-function listarLivros() {
+// Função para exibir e organizar a listagem dos livros
+function listagem() {
   if (biblioteca.length === 0) {
-    console.log('A biblioteca está vazia.');
+    console.log('Não há livros na biblioteca.');
     return;
-  }
+  } 
 
-  const ordenacao = prompt('Ordenar por título (T) ou por data de publicação (D)?');
+  console.log('Ordenar por título ou por data de publicação?')
+  const organizados = prompt('Insira: titulo ou data:');
 
-  let livrosOrdenados;
-  if (ordenacao.toUpperCase() === 'T') {
-    livrosOrdenados = biblioteca.slice().sort((a, b) => a.titulo.localeCompare(b.titulo));
-  } else if (ordenacao.toUpperCase() === 'D') {
-    livrosOrdenados = biblioteca.slice().sort((a, b) => new Date(a.data) - new Date(b.data));
+  let bibliotecaOrganizada; // Fazer a ordenação dos livros
+  if (organizados.toUpperCase() === 'TITULO') {
+    bibliotecaOrganizada = biblioteca.slice().sort((a, b) => a.titulo.localeCompare(b.titulo));
+  } else if (organizados.toUpperCase() === 'DATA') {
+    bibliotecaOrganizada = biblioteca.slice().sort((a, b) => {
+      const dataA = new Date(a.data.split('/').reverse().join('-'));
+      const dataB = new Date(b.data.split('/').reverse().join('-'));
+      return dataB - dataA;
+    }); 
   } else {
-    console.log('Opção inválida.');
+    console.log('Opção inválida');
     return;
   }
 
-  console.log('======== Livros na biblioteca =======');
-  for (const livro of livrosOrdenados) {
+  // Exibir os livros bibliotecaOrganizada
+  console.log('\n//----- Livros na biblioteca -----//');
+  for (const livro of bibliotecaOrganizada) {
     console.log(`Título: ${livro.titulo}`);
     console.log(`Autor: ${livro.autor}`);
     console.log(`Data de publicação: ${livro.data}`);
@@ -45,38 +64,26 @@ function listarLivros() {
   }
 }
 
-function removerLivro() {
-  console.log('======== Remoção de Livro =======');
-  const titulo = prompt('Digite o título do livro que deseja remover:');
-  const index = biblioteca.findIndex(livro => livro.titulo === titulo);
-
-  if (index === -1) {
-    console.log('Livro não encontrado na biblioteca.');
-    return;
-  }
-
-  biblioteca.splice(index, 1);
-  salvarBiblioteca();
-
-  console.log('Livro removido com sucesso!');
-  console.log('=================================\n');
-}
-
-function alterarDetalhesLivro() {
-  console.log('======== Alteração de Detalhes do Livro =======');
+// Função para alterar os detalhes de um livro já cadastrado
+function modificar() {
+  console.log('//----- Alteração de Detalhes de um Livro -----//');
   const titulo = prompt('Digite o título do livro que deseja alterar:');
   const index = biblioteca.findIndex(livro => livro.titulo === titulo);
 
   if (index === -1) {
-    console.log('Livro não encontrado na biblioteca.');
+    console.log('Livro não encontrado.');
     return;
   }
 
   const livro = biblioteca[index];
 
-  const novoTitulo = prompt('Digite o novo título do livro (ou deixe em branco para manter o título atual):');
-  const novoAutor = prompt('Digite o novo autor do livro (ou deixe em branco para manter o autor atual):');
-  const novaData = prompt('Digite a nova data de publicação do livro (formato: DD/MM/AAAA) (ou deixe em branco para manter a data atual):');
+  console.log('\nDeixe em branco o que não quiser alterar')
+  console.log('\nDigite o novo título:');
+  let novoTitulo = prompt('Escreva aqui:');
+  console.log('\nDigite o novo autor:');
+  let novoAutor = prompt('Escreva aqui:');
+  console.log('\nDigite a nova data de publicação no formato: DD/MM/AAAA:');
+  let novaData = prompt('Escreva aqui:');
 
   if (novoTitulo.trim() !== '') {
     livro.titulo = novoTitulo;
@@ -92,64 +99,78 @@ function alterarDetalhesLivro() {
 
   salvarBiblioteca();
 
-  console.log('Detalhes do livro alterados com sucesso!');
-  console.log('=========================================\n');
+  console.log('\nDetalhes do livro alterados com sucesso!');
+  console.log('-------------------------------------------\n');
 }
 
+// Função para remover um livro
+function remover() {
+  console.log('//----- Remoção de um Livro -----//');
+  const titulo = prompt('Digite o título do livro que deseja remover:');
+  const index = biblioteca.findIndex(livro => livro.titulo === titulo);
+
+  if (index === -1) {
+    console.log('Livro não encontrado.');
+    return;
+  }
+
+  biblioteca.splice(index, 1);
+  salvarBiblioteca();
+
+  console.log('Livro removido com sucesso!');
+  console.log('-------------------------------\n');
+}
+
+// Função para salvar os livros em um arquivo .txt
 function salvarBiblioteca() {
-  const conteudo = biblioteca.map(livro => `${livro.titulo};${livro.autor};${livro.data}`).join('\n');
-  fs.writeFileSync(filename, conteudo);
+  const livrosArmazenados = biblioteca.map(livro => `${livro.titulo};${livro.autor};${livro.data}`).join('\n');
+  fs.writeFileSync('livros.txt', livrosArmazenados);
 }
 
+// Função para carregar os livros que foram salvos no arquivo .txt
 function carregarBiblioteca() {
-  if (fs.existsSync(filename)) {
-    const conteudo = fs.readFileSync(filename, 'utf-8');
-    const linhas = conteudo.split('\n');
+  if (fs.existsSync('livros.txt')) {
+    const livrosArmazenados = fs.readFileSync('livros.txt', 'utf-8');
+    const linhas = livrosArmazenados.split('\n');
 
     for (const linha of linhas) {
+      if (linha.trim() === '') {
+    continue;
+  }
       const [titulo, autor, data] = linha.split(';');
       biblioteca.push({ titulo, autor, data });
     }
   }
 }
 
-function exibirMenu() {
-  console.log('======== Gerenciador de livros =======');
-  console.log('1. Cadastrar livro');
-  console.log('2. Listar livros');
-  console.log('3. Remover livro');
-  console.log('4. Alterar detalhes do livro');
-  console.log('5. Sair');
-  console.log('===========================\n');
-}
-
+// Função que inicia todo o funcionamento do programa
 function iniciar() {
   carregarBiblioteca();
 
-  let executando = true;
+  let executando = true; // Funcionará enquanto o usuário realizar suas operações
   while (executando) {
-    exibirMenu();
-    const opcao = prompt('Digite uma opção:');
+    menu();
+    const opcao = prompt('Insira a operação desejada:');
     console.log();
 
     switch (opcao) {
       case '1':
-        cadastrarLivro();
+        cadastro();
         break;
       case '2':
-        listarLivros();
+        listagem();
         break;
       case '3':
-        removerLivro();
+        modificar();
         break;
       case '4':
-        alterarDetalhesLivro();
+        remover();
         break;
       case '5':
         executando = false;
         break;
       default:
-        console.log('Opção inválida.');
+        console.log('Opção inválida');
         break;
     }
 
